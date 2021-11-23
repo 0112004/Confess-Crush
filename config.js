9eb4c390b13276697267f195bd60cf7250258d05
0112004 <94888663+0112004@users.noreply.github.com>
2021-11-23 13:16:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introTitle: 'Chào mừng bạn',
     introDesc: `Nhận làm NV kk mdd zb đến ppkk 
 (giá rẻ, ut tín, chất lượng)
-Buff đậu, thanh lý đồ .... sll...
+Buff đậu, thanh lý đồ .... sll....
 Ngoài ra còn nhận up đệ tử theo yêu cầu.`,
     btnIntro: 'Tôi cần thuê dịch vụ',
     title: 'B hãy liên hệ với chung tôi để tham khảo giá cả ',
