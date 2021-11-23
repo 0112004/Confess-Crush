251f52f3bd145e67291f682b9b5339e3171b3721
0112004 <94888663+0112004@users.noreply.github.com>
2021-11-23 13:08:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,15 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    titleWeb: "Dịch vụ NR online sv2",
+    introTitle: 'Chào mừng bạn',
+    introDesc: `Nhận làm NV kk mdd zb đến ppkk 
+(giá rẻ, ut tín, chất lượng)
+Buff đậu, thanh lý đồ .... sll...
+Ngoài ra còn nhận up đệ tử theo yêu cầu.`,
+    btnIntro: 'Tôi cần thuê dịch vụ',
+    title: 'B hãy liên hệ với chung tôi để tham khảo giá cả ',
+    desc: 'Bạn hãy ấn vào :" ib " để thuê dịch vụ ',
+    btnYes: 'ib',
+    btnNo: 'Không ib',
+   
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
